5e430f3e1ea7c8cce00b48b3662b79c6c21abc55
m0t9_ <matveykorinenko@gmail.com>
2022-07-06 18:34:53 +0500

    Updated /edit_bd interface

--- bot.py
+++ bot.py
@@ -1,26 +1,35 @@
 from telethon.sync import TelegramClient, events
 from telethon import functions
 from telethon.tl.types import ChannelParticipantsSearch
+from telethon.tl.custom import Button
 import datetime
 from apscheduler.schedulers.asyncio import AsyncIOScheduler
 
 import config
 import db_funcs
 
+
 # settings and constants
+class Month:
+    def __init__(self, name, genitive, day_count):
+        self.name = name
+        self.genitive = genitive
+        self.day_count = day_count
+
+
 month_properties = {
-    1: (31, 'января'),
-    2: (29, 'февраля'),
-    3: (31, 'марта'),
-    4: (30, 'апреля'),
-    5: (31, 'мая'),
-    6: (30, 'июня'),
-    7: (31, 'июля'),
-    8: (31, 'августа'),
-    9: (30, 'сентября'),
-    10: (31, 'октября'),
-    11: (30, 'ноября'),
-    12: (31, 'декабря')
+    1: Month('январь', 'января', 31),
+    2: Month('февраль', 'февраля', 29),
+    3: Month('март', 'марта', 31),
+    4: Month('апрель', 'апреля', 30),
+    5: Month('май', 'мая', 31),
+    6: Month('июнь', 'июня', 30),
+    7: Month('июль', 'июля', 31),
+    8: Month('август', 'августа', 31),
+    9: Month('сентябрь', 'сентября', 30),
+    10: Month('октябрь', 'октября', 31),
+    11: Month('ноябрь', 'ноября', 30),
+    12: Month('декабрь', 'декабря', 31)
 }
 
 db_worker = db_funcs.DatabaseWorker(config.DATABASE)
@@ -48,7 +57,7 @@ async def congratulation(mentions, day, month, chat_id):
         word_forms[0] = 'празднует пользователь'
         word_forms[1] = 'его'
 
-    text = f'В этот замечательный день — {day} {month_properties[month][1]} ' \
+    text = f'В этот замечательный день — {day} {month_properties[month].genitive} ' \
            f'свой День рождения {word_forms[0]} {", ".join(mentions)}!\n\nДавайте вместе {word_forms[1]} поздравим 🎉🎉🎉'
 
     await bot.send_message(chat_id, text)
@@ -71,7 +80,7 @@ def create_list(calendar):
             break
 
     for date, users in days_info:
-        day_message = [f'<b>{date[1]} {month_properties[date[0]][1]}</b>', ', '.join(users)]
+        day_message = [f'<b>{date[1]} {month_properties[date[0]].genitive}</b>', ', '.join(users)]  #
         message_blocks.append('\n'.join(day_message))
 
     return '\n\n'.join(message_blocks)
@@ -79,7 +88,7 @@ def create_list(calendar):
 
 # recognize
 def is_date_correct(day, month):
-    return (month in month_properties) and (1 <= day <= month_properties[month][0])
+    return (month in month_properties) and (1 <= day <= month_properties[month].day_count)
 
 
 def is_time_correct(hours, minutes):
@@ -114,31 +123,20 @@ async def remove_birth_date(event):
         await event.reply('Дата Вашего рождения успешно удалена ❌')
 
 
-@bot.on(events.NewMessage(pattern='^/edit_bd(|@chatBirthday_bot)'))
+@bot.on(events.NewMessage(pattern='^/edit_bd(|@chatBirthday_bot)$'))
 async def edit_birth_date(event):
-    args = get_args(event.text)
-    if len(args) == 0:
-        await event.reply(
-            'Для выполнения этой команды необходимо задать дату рождения в формате \'dd.mm\' без кавычек.')
-        return
-    elif len(args) > 1:
-        await event.reply(
-            'Для выполнения этой команды нужен единственный параметр — дата рождения в формате \'dd.mm\' без кавычек.')
-        return
+    sender_id = (await event.get_sender()).id
+    keyboard = list()
+    for row_ind in range(0, 12, 4):
+        keyboard_row = list()
+        for col in range(row_ind, row_ind + 4):
+            keyboard_row.append(Button.inline(month_properties[col + 1].name.capitalize(),
+                                              data=f"{sender_id} birthdate set_month {col + 1} -"))
+        keyboard.append(keyboard_row)
+    keyboard.append([Button.inline('Отмена ❌', data=f"{sender_id} birthdate set_month cancel -")])
 
-    try:
-        birth_day, birth_month = map(int, args[0].split('.'))
-        sender_id = (await event.get_sender()).id
-
-        if not is_date_correct(birth_day, birth_month):
-            await event.reply('К сожалению, введённая дата некорректна 😔')
-            return
-
-        db_worker.update_birth_date(sender_id, birth_day, birth_month)
-        await event.reply(f'Отлично!\nДата Вашего'
-                          f' рождения успешно установлена на {birth_day} {month_properties[birth_month][1]} 🎉')
-    except ValueError:
-        await event.reply('Это не похоже на дату рождения 🤨')
+    await event.reply('<b>Установка (изменение) даты рождения</b>\nВыберите месяц, в который Вы родились',
+                      buttons=keyboard)
 
 
 @bot.on(events.NewMessage(pattern='^/notify_at(|@chatBirthday_bot)'))
@@ -192,24 +190,27 @@ async def show_all_birthdays_in_chat(event):
     if not (await is_user_admin(sender_id, chat_id)):
         return
 
-    chat_members = await bot(functions.channels.GetParticipantsRequest(
-        chat_id, ChannelParticipantsSearch(''), offset=0, limit=10000,
-        hash=0
-    ))
+    try:
+        chat_members = await bot(functions.channels.GetParticipantsRequest(
+            chat_id, ChannelParticipantsSearch(''), offset=0, limit=10000,
+            hash=0
+        ))
 
-    calendar = dict()
-    for member in chat_members.users:
-        if not db_worker.birth_date_exists(member.id):
-            continue
-        birth_day, birth_month = db_worker.get_birth_date(member.id)
-        mention = await create_mention(member.id)
-        if (birth_month, birth_day) in calendar:
-            calendar[(birth_month, birth_day)].append(mention)
-        else:
-            calendar[(birth_month, birth_day)] = [mention]
+        calendar = dict()
+        for member in chat_members.users:
+            if not db_worker.birth_date_exists(member.id):
+                continue
+            birth_day, birth_month = db_worker.get_birth_date(member.id)
+            mention = await create_mention(member.id)
+            if (birth_month, birth_day) in calendar:
+                calendar[(birth_month, birth_day)].append(mention)
+            else:
+                calendar[(birth_month, birth_day)] = [mention]
 
-    message = await event.reply('.')
-    await bot.edit_message(chat_id, message, create_list(calendar))
+        message = await event.reply('.')
+        await bot.edit_message(chat_id, message, create_list(calendar))
+    except ValueError:
+        await event.reply('Произошла ошибка 😔 Возможно, этот чат не является супергруппой.')
 
 
 # bot notification sending
@@ -235,6 +236,44 @@ async def send_notification():
                 users_to_notify_in_chat.append(await create_mention(member.id))
 
         await congratulation(users_to_notify_in_chat, day, month, chat_id)
+
+
+# callback actions
+@bot.on(events.CallbackQuery())
+async def update_interactive_message(event):
+    user_id = event.original_update.user_id
+    message_id = event.original_update.msg_id
+    peer = event.original_update.peer
+    caller, request_type, stage, pick, previous_pick = event.original_update.data.decode('utf-8').split()
+
+    if int(caller) != user_id:
+        return
+
+    if request_type == 'birthdate':
+        if pick == 'cancel':
+            await bot.edit_message(peer, message_id, 'Изменение даты рождения прервано ❌')
+            return
+        if stage == 'set_month':
+            keyboard = list()
+            days = month_properties[int(pick)].day_count
+            for row_ind in range(1, days + 1, 5):
+                keyboard_row = list()
+                for col in range(row_ind, min(row_ind + 5, days + 1)):
+                    keyboard_row.append(Button.inline(f'{col}', data=f'{user_id} birthdate set_day {col} {pick}'))
+                keyboard.append(keyboard_row)
+            keyboard.append([Button.inline('Отмена ❌', data=f"{user_id} birthdate set_day cancel -")])
+            await bot.edit_message(
+                f'<b>Установка (изменение) даты рождения</b>\nВы выбрали месяц {month_properties[int(pick)].name},'
+                f'теперь выберите день Вашего рождения.', buttons=keyboard)
+        elif stage == 'set_day':
+            birth_month = int(previous_pick)
+            birth_day = int(pick)
+
+            db_worker.update_birth_date(user_id, birth_day, birth_month)
+
+            await bot.edit_message(peer, message_id,
+                                   f'Отлично!\nДата Вашего рождения успешно '
+                                   f'установлена на {birth_day} {month_properties[birth_month].genitive} 🎉')
 
 
 # start bot
